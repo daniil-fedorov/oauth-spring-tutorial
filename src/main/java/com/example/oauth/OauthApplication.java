f59e4169c1ee0a326b11bedaf6e2733edbdde089
daniil.fedorov <daniil.fedorov@xdesign.com>
2023-12-11 11:02:27 +0000

    init commit

---
+++ src/main/java/com/example/oauth/OauthApplication.java
@@ -0,0 +1,12 @@
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
